21288750fd8dd4c46383c8cceea8b52156384999
RedietMillion <redietmillion14@gmail.com>
2022-07-19 10:31:18 +0300

    0and1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    j=0
+    j = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
@@ -9,4 +9,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return j
-
